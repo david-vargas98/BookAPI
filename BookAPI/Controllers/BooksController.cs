7a2c561cc3aae93d0de72896fde811d0cbec79c3
david-vargas98 <edgar981125@live.com.mx>
2025-08-12 00:16:53 -0600

    feat: First BooksController Endpoint .
    Implementation of first endpoint as a mock for testing.

--- BookAPI/Controllers/BooksController.cs
+++ BookAPI/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookAPI.Controllers
 {
@@ -7,6 +8,18 @@ namespace BookAPI.Controllers
     [ApiController] // ApiController attribute is used to indicate that this controller responds to web API requests and gives us more functionalities
     public class BooksController : ControllerBase // ControllerBase is used for APIs since angular will handle the views
     {
+        // Mock data for demonstration purposes
+        private Book[] _books = new Book[] // using underscore is a common convention to indicate a private field
+        {
+            new Book { Id = 1, Author= "Author One", Title= "Book One" },
+            new Book { Id = 2, Author= "Author Two", Title= "Book Two" },
+            new Book { Id = 3, Author= "Author Three", Title= "Book Three" }
+        };
 
+        [HttpGet] // This attribute indicates that this method responds to GET requests
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(_books); // Returns a 200 OK response with the list of books
+        }
     }
 }
