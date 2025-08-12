7bf2405ecbc45ea72f00f005da2b185e34b73005
david-vargas98 <edgar981125@live.com.mx>
2025-08-12 00:27:26 -0600

    refactor/feart: Services | Mapping.
    Giving some structure to the application start.
    
    Adding the controller service in order to use the controller I previosuly created.
    
    Also, I've added the controller mapping to automatically handle the routes to the controller.

--- BookAPI/Program.cs
+++ BookAPI/Program.cs
@@ -4,10 +4,17 @@ namespace BookAPI
     {
         public static void Main(string[] args)
         {
+            // This is creating the builder for the web application
             var builder = WebApplication.CreateBuilder(args);
+
+            // Here we can add additional services to the builder
+            builder.Services.AddControllers(); // we tell our application that we've created controllers and we want to use them inside the application
+
+            // Then, this is build the web application startup
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            // Here we can add mapping for routes and other configurations
+            app.MapControllers(); // with this, everything will be automatically mapped to the controllers we created
 
             app.Run();
         }
