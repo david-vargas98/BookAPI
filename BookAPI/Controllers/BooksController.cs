f1b5de4015c83a5681a58a493d078b5bf9ea1ead
david-vargas98 <edgar981125@live.com.mx>
2025-08-12 00:01:10 -0600

    feat: Controller Configs.
    Affing basic configurations to controller.

--- BookAPI/Controllers/BooksController.cs
+++ BookAPI/Controllers/BooksController.cs
@@ -1,6 +1,12 @@
-﻿namespace BookAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
 {
-    public class BooksController
+    // It'll look like: https://localhost:7050/api/books
+    [Route("api/[Controller]")] // Route attribute defines the base route for the controller
+    [ApiController] // ApiController attribute is used to indicate that this controller responds to web API requests and gives us more functionalities
+    public class BooksController : ControllerBase // ControllerBase is used for APIs since angular will handle the views
     {
+
     }
 }
